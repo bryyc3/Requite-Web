fd3603a6f3690081e8e32f7d9694bed06ff73bad
Bryce King <bryceeking5@gmail.com>
2026-05-23 22:04:24 -0700

    Text sizing for home carousel, gradient borders for tier customization and tracking system components.

--- app/(dashboard)/dashboard/tracking-system/components/TrackingSysContainer.tsx
+++ app/(dashboard)/dashboard/tracking-system/components/TrackingSysContainer.tsx
@@ -9,12 +9,14 @@ type TrackingSysContainerProps = {
 
 export default function TrackingSysContainer({system}: TrackingSysContainerProps) {
     return(
-        <div className="shadow-[0_8px_8px_rgba(0,0,0,0.55)] bg-white rounded-2xl p-10">
-            <div className="flex items-center w-full pb-10 gap-[clamp(0.5rem,10vw,50rem)]">
-                <h1>{system.name}</h1>
+      <div className="rounded-xl bg-gradient-to-r from-orange-500 via-orange-500 to-orange-300 p-[2px] shadow-[0_8px_8px_rgba(0,0,0,0.55)] ">
+        <div className="bg-white rounded-[10px] p-7">
+            <div className="flex justify-between items-center pb-10">
+                <h1 className="font-bold text-[clamp(.1rem,2cqi,1.5rem)]">{system.name}</h1>
                 <ToggleButton />
             </div>
-            <p>{system.description}</p>
+            <p className="font-extralight text-[clamp(.3rem,1.5cqi,1rem)] pb-10">{system.description}</p>
         </div>
+      </div>
     )
 }
